7ee2954f63ca88985bfde6c8e726d1ab29476fa5
Geony <geony8410@gmail.com>
2022-01-24 02:04:30 +0900

    second

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,70 +3,72 @@ const form = document.getElementById("commentForm");
 const deleteBtns = document.querySelectorAll("#commentDelete");
 
 const handleClickDelete = async (event) => {
-  const videoComment = event.target.parentNode;
-  const videoId = videoContainer.dataset.videoid;
-  const commentId = videoComment.dataset.commentid;
-  const { status } = await fetch(`/api/videos/${videoId}/comment/delete`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ commentId }),
-  });
-  if (status === 200) {
-    videoComment.remove();
-  }
+	const videoComment = event.target.parentNode;
+	const videoId = videoContainer.dataset.videoid;
+	const commentId = videoComment.dataset.commentid;
+	const { status } = await fetch(`/api/videos/${videoId}/comment/delete`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ commentId }),
+	});
+	if (status === 200) {
+		videoComment.remove();
+	}
 };
 
-const addComment = (text) => {
-  const videoComments = document.querySelector(".video__comments ul");
-  const newComment = document.createElement("li");
-  newComment.className = "video__comment";
-  const icon = document.createElement("i");
-  icon.className = "fas fa-comment";
-  const span = document.createElement("span");
-  span.innerText = `  ${text}`;
-  const button = document.createElement("button");
-  button.id = "commentDelete";
-  button.innerText = "❌";
-  button.addEventListener("click", handleClickDelete);
-  newComment.appendChild(icon);
-  newComment.appendChild(span);
-  newComment.appendChild(button);
-  videoComments.prepend(newComment);
+const addComment = (text, id) => {
+	const videoComments = document.querySelector(".video__comments ul");
+	const newComment = document.createElement("li");
+	newComment.className = "video__comment";
+	newComment.setAttribute("data-commentId", id);
+	const icon = document.createElement("i");
+	icon.className = "fas fa-comment";
+	const span = document.createElement("span");
+	span.innerText = `  ${text}`;
+	const button = document.createElement("button");
+	button.id = "commentDelete";
+	button.innerText = "❌";
+	button.addEventListener("click", handleClickDelete);
+	newComment.appendChild(icon);
+	newComment.appendChild(span);
+	newComment.appendChild(button);
+	videoComments.prepend(newComment);
 };
 
 const handleSubmit = async (event) => {
-  event.preventDefault();
-  const textarea = form.querySelector("textarea");
-  const text = textarea.value;
-  const videoId = videoContainer.dataset.videoid;
-  if (text === "") {
-    return;
-  }
-  // status = fetch-promise.status
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text }),
-  }); // fetch :: send request to backend
-  // fetch 로 백엔드에 request 해서 data 가 돌아오는데 시간이 걸리기 때문에 await 을 해주는게 좋다.
-  textarea.value = "";
-  if (status === 201) {
-    // create success status code
-    addComment(text);
-  }
+	event.preventDefault();
+	const textarea = form.querySelector("textarea");
+	const text = textarea.value;
+	const videoId = videoContainer.dataset.videoid;
+	if (text === "") {
+		return;
+	}
+	// status = fetch-promise.status
+	const response = await fetch(`/api/videos/${videoId}/comment`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ text }),
+	}); // fetch :: send request to backend
+	// fetch 로 백엔드에 request 해서 data 가 돌아오는데 시간이 걸리기 때문에 await 을 해주는게 좋다.
+	const { status } = response;
+	const result = await response.json();
+	if (status === 201) {
+		addComment(text, result.id);
+	}
+	textarea.value = "";
 };
 // fetch 만 하면 쿠키는 자동으로 브라우저에 전송되고 백엔드에서 사용할 수 있음
 
 if (form) {
-  // login 을 해야 form 이 생성됨
-  form.addEventListener("submit", handleSubmit);
-  // delete Btn don't exists
-  for (const btn of deleteBtns) {
-    btn.addEventListener("click", handleClickDelete);
-  }
+	// login 을 해야 form 이 생성됨
+	form.addEventListener("submit", handleSubmit);
+	// delete Btn don't exists
+	for (const btn of deleteBtns) {
+		btn.addEventListener("click", handleClickDelete);
+	}
 }
 // btn click event 대신에 form 에 submit event 를 감지하는 것이 더 좋다.
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,204 +4,206 @@ import User from "../models/User";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const pageTitle = "Home";
-  const videos = await Video.find({})
-    .sort({ createdAt: "desc" })
-    .populate("owner");
-  if (!videos) {
-    return res.render("home", {
-      pageTitle,
-      errorMessage: "Video not found.",
-    });
-  }
-  return res.render("home", { pageTitle, videos });
+	const pageTitle = "Home";
+	const videos = await Video.find({})
+		.sort({ createdAt: "desc" })
+		.populate("owner");
+	if (!videos) {
+		return res.render("home", {
+			pageTitle,
+			errorMessage: "Video not found.",
+		});
+	}
+	return res.render("home", { pageTitle, videos });
 };
 
 export const watch = async (req, res) => {
-  const {
-    params: { id },
-  } = req;
-  const video = await Video.findById(id).populate("owner").populate("comments");
-  if (!video) {
-    return res.render("watch", {
-      pageTitle,
-      errorMessage: "Video does not exsists",
-    });
-  }
-  return res.render("watch", { pageTitle: video.title, video });
+	const {
+		params: { id },
+	} = req;
+	const video = await Video.findById(id)
+		.populate("owner")
+		.populate("comments");
+	if (!video) {
+		return res.render("watch", {
+			pageTitle,
+			errorMessage: "Video does not exsists",
+		});
+	}
+	return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = async (req, res) => {
-  const {
-    params: { id },
-    session: {
-      user: { _id },
-    },
-  } = req;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.status(404).render("404", { pageTitle: "Video not found." });
-  }
-  if (String(video.owner) !== String(_id)) {
-    req.flash("error", "You are not the owner of this video.");
-    return res.status(403).redirect("/");
-  }
-  return res.render("edit-video", {
-    pageTitle: `Editing : ${video.title}`,
-    video,
-  });
+	const {
+		params: { id },
+		session: {
+			user: { _id },
+		},
+	} = req;
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.status(404).render("404", { pageTitle: "Video not found." });
+	}
+	if (String(video.owner) !== String(_id)) {
+		req.flash("error", "You are not the owner of this video.");
+		return res.status(403).redirect("/");
+	}
+	return res.render("edit-video", {
+		pageTitle: `Editing : ${video.title}`,
+		video,
+	});
 };
 export const postEdit = async (req, res) => {
-  const {
-    session: {
-      user: { _id },
-    },
-    params: { id },
-    body: { title, description, hashtags },
-  } = req;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.status(404).render("404", { pageTitle: "Video not found." });
-  }
-  if (String(video.owner) !== String(_id)) {
-    return res.status(403).redirect("/");
-  }
-  await Video.findByIdAndUpdate(id, {
-    title,
-    description,
-    hashtags: Video.formatHashtags(hashtags),
-  });
-  req.flash("success", "Changes saved");
-  return res.redirect(`/videos/${id}`);
+	const {
+		session: {
+			user: { _id },
+		},
+		params: { id },
+		body: { title, description, hashtags },
+	} = req;
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.status(404).render("404", { pageTitle: "Video not found." });
+	}
+	if (String(video.owner) !== String(_id)) {
+		return res.status(403).redirect("/");
+	}
+	await Video.findByIdAndUpdate(id, {
+		title,
+		description,
+		hashtags: Video.formatHashtags(hashtags),
+	});
+	req.flash("success", "Changes saved");
+	return res.redirect(`/videos/${id}`);
 };
 
 export const deleteVideo = async (req, res) => {
-  const pageTitle = "Edit Video";
-  const {
-    session: {
-      user: { _id },
-    },
-    params: { id },
-  } = req;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.render("edit-video", {
-      pageTitle,
-      errorMessage: "Video Not exists",
-    });
-  }
-  if (String(video.owner) !== String(_id)) {
-    return res.redirect("/");
-  }
-  await Video.findByIdAndRemove(id);
-  return res.redirect("/");
+	const pageTitle = "Edit Video";
+	const {
+		session: {
+			user: { _id },
+		},
+		params: { id },
+	} = req;
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.render("edit-video", {
+			pageTitle,
+			errorMessage: "Video Not exists",
+		});
+	}
+	if (String(video.owner) !== String(_id)) {
+		return res.redirect("/");
+	}
+	await Video.findByIdAndRemove(id);
+	return res.redirect("/");
 };
 
 export const getUpload = (req, res) => {
-  const pageTitle = "Upload Video";
-  return res.render("upload", { pageTitle });
+	const pageTitle = "Upload Video";
+	return res.render("upload", { pageTitle });
 };
 export const postUpload = async (req, res) => {
-  const pageTitle = "Upload Video";
-  const {
-    body: { title, description, hashtags },
-    session: {
-      user: { _id },
-    },
-    files: { video, thumb },
-  } = req;
+	const pageTitle = "Upload Video";
+	const {
+		body: { title, description, hashtags },
+		session: {
+			user: { _id },
+		},
+		files: { video, thumb },
+	} = req;
 
-  try {
-    const newVideo = await Video.create({
-      title,
-      description,
-      fileUrl: video[0].path,
-      thumbUrl: thumb[0].path,
-      createdAt: new Date(),
-      hashtags: Video.formatHashtags(hashtags),
-      owner: _id,
-    });
-    const user = await User.findById(_id);
-    user.videos.push(newVideo._id);
-    user.save();
-    return res.redirect("/");
-  } catch (error) {
-    return res.render("upload", {
-      pageTitle,
-      errorMessage: error._message,
-    });
-  }
+	try {
+		const newVideo = await Video.create({
+			title,
+			description,
+			fileUrl: video[0].path,
+			thumbUrl: thumb[0].path,
+			createdAt: new Date(),
+			hashtags: Video.formatHashtags(hashtags),
+			owner: _id,
+		});
+		const user = await User.findById(_id);
+		user.videos.push(newVideo._id);
+		user.save();
+		return res.redirect("/");
+	} catch (error) {
+		return res.render("upload", {
+			pageTitle,
+			errorMessage: error._message,
+		});
+	}
 };
 
 export const search = async (req, res) => {
-  const pageTitle = "Search the videos!";
-  const {
-    query: { keyword },
-  } = req;
-  let videos = [];
-  let errorMessage;
-  if (keyword) {
-    videos = await Video.find({
-      title: {
-        $regex: new RegExp(keyword, "i"),
-      },
-    }).populate("owner");
-    if (videos.length === 0) {
-      errorMessage = "Video not found";
-    }
-  }
-  return res.render("search", { pageTitle, videos, errorMessage });
+	const pageTitle = "Search the videos!";
+	const {
+		query: { keyword },
+	} = req;
+	let videos = [];
+	let errorMessage;
+	if (keyword) {
+		videos = await Video.find({
+			title: {
+				$regex: new RegExp(keyword, "i"),
+			},
+		}).populate("owner");
+		if (videos.length === 0) {
+			errorMessage = "Video not found";
+		}
+	}
+	return res.render("search", { pageTitle, videos, errorMessage });
 };
 
 export const registerView = async (req, res) => {
-  const {
-    params: { id },
-  } = req;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.sendStatus(404);
-  }
-  video.meta.views = video.meta.views + 1;
-  await video.save();
-  return res.sendStatus(200);
+	const {
+		params: { id },
+	} = req;
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.sendStatus(404);
+	}
+	video.meta.views = video.meta.views + 1;
+	await video.save();
+	return res.sendStatus(200);
 };
 
 export const createComment = async (req, res) => {
-  const {
-    params: { id },
-    session: { user },
-    body: { text },
-  } = req;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.sendStatus(404); // send and kill request
-  }
-  const dbUser = await User.findById({ _id: user._id });
-  const comment = await Comment.create({
-    text,
-    owner: user._id,
-    video: id,
-  });
-  video.comments.push(comment._id);
-  dbUser.comments.push(comment._id);
-  await video.save();
-  await dbUser.save();
-  return res.sendStatus(201);
+	const {
+		params: { id },
+		session: { user },
+		body: { text },
+	} = req;
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.sendStatus(404); // send and kill request
+	}
+	const dbUser = await User.findById({ _id: user._id });
+	const comment = await Comment.create({
+		text,
+		owner: user._id,
+		video: id,
+	});
+	video.comments.push(comment._id);
+	dbUser.comments.push(comment._id);
+	await video.save();
+	await dbUser.save();
+	return res.status(201).json({ id: comment._id });
 };
 
 export const deleteComment = async (req, res) => {
-  const {
-    params: { id },
-    body: { commentId },
-  } = req;
-  const video = await Video.findById(id);
-  if (!video) {
-    return res.sendStatus(404);
-  }
-  const comment = await Comment.findById({ _id: commentId });
-  if (!comment) {
-    return res.sendStatus(404);
-  }
-  await Comment.findByIdAndRemove({ _id: commentId });
-  return res.sendStatus(200);
+	const {
+		params: { id },
+		body: { commentId },
+	} = req;
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.sendStatus(404);
+	}
+	const comment = await Comment.findById({ _id: commentId });
+	if (!comment) {
+		return res.sendStatus(404);
+	}
+	await Comment.findByIdAndRemove({ _id: commentId });
+	return res.sendStatus(200);
 };
